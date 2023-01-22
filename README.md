0cb64720e4a2b513a8b97021dd235bb3847186d3
Jeroen Overschie <jeroenoverschie@gmail.com>
2023-01-22 14:52:58 +0100

    Make template more minimal + polish 💎

--- README.md
+++ README.md
@@ -7,19 +7,50 @@ Hi! This is a template you can generate a Bookshelf website with.
 
 ## Usage
 
+
+### 
+1. Click _Use this template_ to create a new repo.
+
+    <a href="https://github.com/dunnkers/bookshelf-template/generate"><img alt="use this template GitHub button" src="https://camo.githubusercontent.com/523d7e81c3d3dcd01b711f14c87e850edeb8e62bf72814d3231ab084a0c70d31/68747470733a2f2f7777772e726f737472756d2e626c6f672f706f73742f323031392d30362d31312d612d7265706f2d74656d706c6174652d666f722d722d616e616c797369735f66696c65732f7573652d746869732e706e67" width="150px" /></a>
+
+
 1. Install deps:
 
     ```
     yarn
     ```
 
-2. Build the website:
+1. Build the website:
 
     ```
     yarn build
     ```
 
+    ✨
+
     This now put a static website in `build`. You can deploy this to GitHub pages.
+
+### Loading your own books
+To now load your own books onto the bookshelf, go into `compile-website.mjs` and load your own books into the `bookShelves` variable.
+
+```javascript
+const bookShelves = null; // Load your books into this variable.
+```
+
+You can, for example, organise your books on [OpenLibrary.org](https://openlibrary.org/) and then retrieve your books using their awesome free [API](https://openlibrary.org/developers/api). If you happen to have a Goodreads API key then that is possible too, check out the [`goodreads-api`](https://github.com/dunnkers/goodreads-api). However, Goodreads [no longer issues API keys](https://help.goodreads.com/s/article/Does-Goodreads-support-the-use-of-APIs) for new users.
+
+## Deploying
+
+### GitHub pages
+To deploy to GitHub pages, check out the file at `.github/workflows/update-website.yml`. It is possible to add a recurring schedule to periodically push a new bookshelf website. To do that, add the following:
+
+```yaml
+on:
+    schedule:
+        - cron: "0 12 * * *"
+```
+
+To update the bookshelf every day at 12. See [this link](https://github.com/dunnkers/bookshelf/blob/master/.github/workflows/update-website.yml#L4-L5) for a workflow using this schedule.
 
 ## About
 Built by [Jeroen Overschie](https://jeroenoverschie.nl/).
--- compile-website.mjs
+++ compile-website.mjs
@@ -1,59 +1,51 @@
-import handlebars from 'handlebars';
-import { readFileSync, writeFileSync } from 'fs';
+import handlebars from "handlebars";
+import { readFileSync, writeFileSync } from "fs";
 
-const books = {
-    "current": {
+// Books
+// -> Replace this with your own data (e.g. fetch from an API)
+const bookShelves = [
+    {
+        "name": "Currently reading",
         "books": [
             {
-                "book": {
-                    "image_url": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1626718328l/57933306.jpg",
-                    "title": "Stolen Focus",
-                    "link": "https://www.goodreads.com/book/show/57933306-stolen-focus",
-                    "authors": {
-                        "author": {
-                            "name": "Johann Hari"
-                        }
-                    }
-                }
+                "title": "Stolen Focus",
+                "author": "Johann Hari",
+                "link": "https://www.goodreads.com/book/show/57933306-stolen-focus",
+                "image_url": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1626718328l/57933306.jpg"
             }
         ],
     },
-
-    "read": {
+    {
+        "name": "Read",
         "books": [
             {
-                "book": {
-                    "image_url": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1589926186l/9700791._SY475_.jpg",
-                    "title": "Man's Search for Meaning",
-                    "link": "https://www.goodreads.com/book/show/9700791-man-s-search-for-meaning",
-                    "authors": {
-                        "author": {
-                            "name": "Viktor E. Frankl"
-                        }
-                    }
-                }
+                "title": "Man's Search for Meaning",
+                "author": "Viktor E. Frankl",
+                "link": "https://www.goodreads.com/book/show/9700791-man-s-search-for-meaning",
+                "image_url": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1589926186l/9700791._SY475_.jpg"
             }
         ],
     }
+]
+
+// Variables
+const publicURL = process.env.PUBLIC_URL || ""; // e.g. `http://dunnkers.github.io/`
+const dateNow = new Date();
+const dateString = dateNow.toDateString();
+const dateYear = dateNow.getFullYear();
+const dateTimeString = dateNow.toTimeString();
+
+// Compile Handlebars 
+const handlebarsTemplate = readFileSync("templates/index.hbs", "utf8");
+const handlebarsCompiled = handlebars.compile(handlebarsTemplate);
+const handlebarsContext = {
+    "bookShelves": bookShelves,
+    "publicURL": publicURL,
+    "dateString": dateString,
+    "dateYear": dateYear,
+    "dateTimeString": dateTimeString
 }
+const html = handlebarsCompiled(handlebarsContext);
 
-
-const today = new Date();
-const todayString = today.toDateString();
-const todayTime = today.toTimeString();
-const year = today.getFullYear();
-
-// Compile said template
-var template = readFileSync('templates/index.hbs', 'utf8');
-var compiled = handlebars.compile(template);
-var html = compiled({
-    PUBLIC_URL: process.env.PUBLIC_URL || "",
-    year,
-    "read": books["read"].books,
-    "current": books["current"].books,
-    todayString,
-    todayTime
-});
-
-// Write HTML file
-writeFileSync('build/index.html', html);
+// Write HTML
+writeFileSync("build/index.html", html);
